301bf2a3aef8ad169cab4da1c3ff9f74da0909ae
Fabian Streit(Leisure) <fabianstreitleisure@Fabians-MacBook-Pro.local>
2022-10-26 15:03:54 +0200

    add tests for ressource teilnehmer

--- test.py
+++ test.py
@@ -1,14 +1,29 @@
-from stat import FILE_ATTRIBUTE_READONLY
+'''
+In der test.py Datei, werden die Methoden für die Teilnehmer Ressource getestet
+'''
 from urllib import response
 import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response=requests.get(BASE + "teilnehmer/1")
+response=requests.post(BASE + "teilnehmer/1", {"id": 1, "vorname": "Fabian", "name": "Streit",
+ "thema": "APIs im Backend", "session_chair":"Quang", "note":0.0}, timeout=5)
+input()
+
+response=requests.get(BASE + "teilnehmer/1", timeout=5)
 print(response.json())
 input()
 
-response=requests.patch(BASE + "teilnehmer/1", {"thema":"APIs im Backend"})
+response=requests.put(BASE + "teilnehmer/1", {"vorname": "Fabian", "name": "Streit",
+ "thema": "APIs im Backend mit Flask", "session_chair":"Quang", "note":0.0}, timeout=5)
 print(response.json())
+input()
+
+response=requests.patch(BASE + "teilnehmer/1", {"thema":"APIs im Backend mit Flask und Django"},
+ timeout=5)
+print(response.json())
+input()
+
+response=requests.delete(BASE + "teilnehmer/1", timeout=5)
 
 input()
